336f222ba17e6b9b61ea82bb54a36835e1cf9bee
Samuel Wu <twopizza9621536@gmail.com>
2025-03-28 18:36:17 -0400

    Add fallback if the bat command was renamed to bat
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>

--- functions/main.zsh
+++ functions/main.zsh
@@ -1,8 +1,17 @@
 # define some functions to avoid any error that these programs don't exist
 # treat bat -lXXX as cat
 # currently, only bat supports highlight --help
-alias -g -- "--help=\\--help | bat -lhelp"
-(($+commands[bat])) && bat() {command bat --color=always -p $@} || bat() {command cat}
+
+if (($+commands[bat])); then
+  alias -g -- "--help=\\--help | bat -lhelp"
+  bat() {command bat --color=always -p $@}
+elif (($+commands[batcat])); then
+  alias -g -- "--help=\\--help | batcat -lhelp"
+  bat() {command batcat --color=always -p $@}
+else
+  bat() {command cat}
+fi
+
 if ((! $+commands[mdcat])); then
   if (($+commands[paper])); then
     mdcat() {command paper $@}
@@ -13,12 +22,15 @@ if ((! $+commands[mdcat])); then
     mdcat() {bat -lmarkdown}
   fi
 fi
+
 if ((! $+commands[finger])); then
   (($+commands[pinky])) && finger() {command pinky $@} ||
     finger() {command whoami}
 fi
+
 (($+commands[pandoc])) || pandoc() {command cat ${@[-1]}}
 (($+commands[grc])) || grc() {eval ${@[2,-1]}}
+
 # https://github.com/Freed-Wu/fzf-tab-source/issues/6
 if (($+commands[less])) && [ -x ~/.lessfilter ]; then
   less() {~/.lessfilter $@ || command less $@}
